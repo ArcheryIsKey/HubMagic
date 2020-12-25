5f5b3ceb256fca2de9755f098dddb522d9076449
ArcheryIsKey <kinetix0000@gmail.com>
2020-12-25 05:12:39 -0600

    Rewrote wardrobe code

--- me/Archery/hubmagic/Gadgets/Wardrobe.java
+++ me/Archery/hubmagic/Gadgets/Wardrobe.java
@@ -56,99 +56,26 @@ public class Wardrobe implements Listener
             ItemStack clicked = e.getCurrentItem();
             if(e.getInventory().equals(HubMagic.plugin.ward) && clicked != null) {
             	e.setCancelled(true);
-            switch(clicked.getType()) {
-            
-			case BARRIER:
-				p.getInventory().setHelmet(null);
-	            p.getInventory().setChestplate(null);
-	            p.getInventory().setLeggings(null);
-	            p.getInventory().setBoots(null);
-	            
-	            break;
-				
-			case LEATHER_HELMET:
-                p.getInventory().setHelmet(new ItemStack(Material.LEATHER_HELMET));
-                break;
-            
-			case GOLDEN_HELMET:
-                p.getInventory().setHelmet(new ItemStack(Material.GOLDEN_HELMET));
-                 break;
-            
-			case IRON_HELMET:
-                p.getInventory().setHelmet(new ItemStack(Material.IRON_HELMET));
-                break;
-            
-			case DIAMOND_HELMET:
-                p.getInventory().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-                break;
-            
-			case CHAINMAIL_HELMET:
-                p.getInventory().setHelmet(new ItemStack(Material.CHAINMAIL_HELMET));
-                break;
-            
-			case LEATHER_CHESTPLATE:
-                p.getInventory().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
-                break;
-            
-			case GOLDEN_CHESTPLATE:
-                p.getInventory().setChestplate(new ItemStack(Material.GOLDEN_CHESTPLATE));
-                break;
-             
-			case IRON_CHESTPLATE:
-                p.getInventory().setChestplate(new ItemStack(Material.IRON_CHESTPLATE));
-                break;
-            
-			case DIAMOND_CHESTPLATE:
-                p.getInventory().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-                break;
-            
-			case CHAINMAIL_CHESTPLATE:
-                p.getInventory().setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
-                break;
-            
-            case GOLDEN_LEGGINGS:
-                p.getInventory().setLeggings(new ItemStack(Material.GOLDEN_LEGGINGS));
-                break;
-             
-            case IRON_LEGGINGS:
-                p.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
-                break;
-            
-            case DIAMOND_LEGGINGS:
-                p.getInventory().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
-                break;
-            
-            case CHAINMAIL_LEGGINGS:
-                p.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
-                break;
-            
-            case LEATHER_LEGGINGS:
-                p.getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
-                break;
-            
-            case LEATHER_BOOTS:
-                p.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS));
-                break;
-            
-            case GOLDEN_BOOTS:
-                p.getInventory().setBoots(new ItemStack(Material.GOLDEN_BOOTS));
-                break;
-            
-            case IRON_BOOTS:
-                p.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
-                break;
-            
-            case DIAMOND_BOOTS:
-                p.getInventory().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-                break;
-            
-            case CHAINMAIL_BOOTS:
-                p.getInventory().setBoots(new ItemStack(Material.CHAINMAIL_BOOTS));
-                break;
-                
-            default: break;
-               }
+            	setWardrobe(p, clicked);
             }
         }
+    }
+    
+    private void setWardrobe(Player p, ItemStack item) {
+    	if(item.getType().name().endsWith("_HELMET")) {
+    		p.getInventory().setHelmet(item);
+    }
+    	if(item.getType().name().endsWith("_CHESTPLATE")) {
+    		p.getInventory().setChestplate(item);
+    }
+		if(item.getType().name().endsWith("_LEGGINGS")) {
+			p.getInventory().setLeggings(item);
+	}
+		if(item.getType().name().endsWith("_BOOTS")) {
+			p.getInventory().setBoots(item);
+		}
+		if(item.getType() == Material.BARRIER) {
+			p.getInventory().setArmorContents(null);
+		}
     }
 }
